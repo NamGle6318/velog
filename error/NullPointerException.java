b6c1d06dba50c6acaee3841b777dc9c968c4ba13
HyunWoo <mogs223@gmail.com>
2025-03-09 19:03:28 +0900

    NPE 발생 예시

--- error/NullPointerException.java
+++ error/NullPointerException.java
@@ -1,9 +1,31 @@
 package error;
 
-
-
 public class NullPointerException {
+    static class Person {
+        String name = null;
+    }
+    public static String getName() {
+        return null;
+    }
+
     public static void main(String[] args) {
-    
+        // NPE(NullPointerException) 에러 발생 예시
+        String name = getName(); 
+        if(name != null)
+        System.out.println(name.length()); // NPE 발생
+        else
+        System.out.println("name = null");
+
+        // Person person = null;
+        // System.out.println(person.name);
+
+        // String str = null;
+        // System.out.println(str.length()); // NPE 발생
+
+        // String[] arr = new String[3]; // 초기값 null
+        // System.out.println(arr[0].length()); // NPE 발생
+
+        
+
     }
 }
